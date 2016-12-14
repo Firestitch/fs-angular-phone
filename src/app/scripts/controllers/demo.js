90fa6c6bdecced5440fb7fbe4bc1afb2b8e0fcae
Raymond <ray@firestitch.com>
2016-12-13 21:46:22 -0500

    initial add

---
+++ src/app/scripts/controllers/demo.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+angular.module('app')
+  .controller('DemoCtrl', function ($scope) {
+
+
+    $scope.disabled = false;
+    $scope.required = false;
+    $scope.phone = "4168884447";
+
+    $scope.submit = function() {
+        alert('submit');
+    }
+});
+
---
+++ src/app/scripts/directives/directive.js
@@ -0,0 +1,129 @@
+(function () {
+    'use strict';
+
+	/**
+	 * @ngdoc directive
+	 * @name fs.directives:fs-phone
+	 * @restrict E
+	 * @param {object} fsModel The modal object. The format should be in minnutes
+	 * @param {string} fsLabel The input label
+	 * @param {string} fsClass The css class pass on to the md-input-container
+	 * @param {expression} fsDisabled An expression to enable/disable the input
+	 * @param {expression} fsRequired An expression to require the input for valiation
+	 */
+    angular.module('fs-angular-phone',['fs-angular-util','fs-angular-validate'])
+    .directive('fsPhone', function($filter, fsUtil, fsValidate, $timeout) {
+        return {
+            templateUrl: 'views/directives/phone.html',
+            restrict: 'E',
+            replace: true,
+            scope: {
+              model: '=?fsModel',
+              label: '@?fsLabel',
+              disabled: '=?fsDisabled',
+              required: '=?fsRequired',
+              class: '@?fsClass'
+            },
+            link: function($scope, element, attrs, ctrl) {
+
+	            var input = angular.element(element[0].querySelector('input[type="text"]'))[0];
+	            $scope.name = 'input_' + fsUtil.guid();
+				$scope.input = '';
+
+				$scope.change = function(e) {
+
+				    var val = angular.element(input).val(),
+				    	start = input.selectionStart,
+				    	end = input.selectionEnd;
+
+				    update(val);
+
+				    if(val.length>start) {
+
+					    $timeout(function () {
+					    	input.setSelectionRange(start, end);
+					    },5);
+					}
+
+	            	$scope.model = $scope.input.replace(/[^0-9]/g,'');
+	            }
+
+	            $scope.keydown = function(e) {
+
+	            	var codes = [	35, //end
+	            					36, //home
+	            					37, //back
+	            					39, //forward
+	            					46, //delete
+	            					8]; //backspace
+
+	            	if(!e.shiftKey && !e.ctrlKey && codes.indexOf(e.keyCode)<0 && !e.key.match(/[\d\(\)]/)) {
+	            		e.preventDefault();
+	            	}
+	            }
+
+	            angular.element(input).data('scope',$scope);
+
+	            $scope.validate = function(value) {
+
+	            	if(!fsValidate.phone(value)) {
+	            		return 'Invalid phone number';
+	            	}
+
+	            	return true;
+	            }
+
+	            function update(value) {
+	            	if(!value) {
+	            		value = "";
+	            	}
+
+	            	$scope.input = $filter('tel')(value.replace(/[^0-9]/g, ''), false);
+	            }
+
+	            update($scope.model);
+            }
+        };
+    })
+    .filter('tel', function () {
+	    return function (tel) {
+	        if (!tel) { return ''; }
+
+	        var value = tel.toString().trim().replace(/^\+/, '');
+
+	        if (value.match(/[^0-9]/)) {
+	            return tel;
+	        }
+
+	        var country, city, number;
+
+	        switch (value.length) {
+	            case 1:
+	            case 2:
+	            case 3:
+	                city = value;
+	                break;
+
+	            default:
+	                city = value.slice(0, 3);
+	                number = value.slice(3);
+	        }
+
+	        if(number){
+	            if(number.length>3){
+	                number = number.slice(0, 3) + '-' + number.slice(3,7);
+	            }
+	            else{
+	                number = number;
+	            }
+
+	            return ("(" + city + ") " + number).trim();
+	        }
+	        else{
+	            return "(" + city;
+	        }
+
+	    };
+	});
+})();
+
